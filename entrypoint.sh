f5524f061c35b459e777d79228a198c355b6c472
Michael McHolm <mcholmm@gmail.com>
2022-01-16 21:36:17 +0000

    automated local testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-exec /usr/sbin/php-fpm8.0 -F
+if [[ "$1" -eq "tests" ]];then
+  while inotifywait -e modify --exclude ".swp$" -r src tests; do
+    phpunit
+  done
+else
+  exec /usr/sbin/php-fpm8.0 -F
+fi
