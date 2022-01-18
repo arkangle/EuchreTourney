45555d7324e7a5a98ba5749804e92798a3cc1d11
Michael McHolm <mcholmm@gmail.com>
2022-01-18 00:49:06 +0000

    improve inotifywait

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ "$1" -eq "tests" ]];then
-  while inotifywait -e modify --exclude ".swp$" -r src tests; do
+  while inotifywait -e modify --exclude ".swp$" -r src tests 2> /dev/null; do
+    echo ============================================================
     phpunit
   done
 else
